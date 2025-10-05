bb4b1056359953ba7f201543fdc2adb938846b03
Juan Diego Pardo <pardilloxd@gmail.com>
2025-10-04 23:53:54 -0500

    terminado again

--- ejercicio5.js
+++ ejercicio5.js
@@ -5,4 +5,8 @@ for (let i = palabra.length -1; i>=0; i--)
     {
         palabraAlReves += palabra[i];
     }
-    console.log(palabraAlReves);
+    if (palabraAlReves === palabra){
+        console.log("La palabra", palabra, "es un palindromo")
+    } else {
+        console.log("La palabra", palabra, "no es un palindromo")
+    }
